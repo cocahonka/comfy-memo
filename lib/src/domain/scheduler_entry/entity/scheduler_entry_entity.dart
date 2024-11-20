55b7d24db8456b299dcc8776bdd1da244f8cff52
cocahonka <cocahonka@gmail.com>
2024-11-21 00:08:09 +0300

    add FSRS algorithm entities and repository interface

---
+++ lib/src/domain/scheduler_entry/entity/scheduler_entry_entity.dart
@@ -0,0 +1,33 @@
+import 'package:meta/meta.dart';
+
+@immutable
+class SchedulerEntryEntity {
+  const SchedulerEntryEntity({
+    required this.cardId,
+    required this.due,
+    required this.scheduledDays,
+    required this.elapsedDays,
+    required this.reps,
+    required this.lapses,
+    required this.lastReview,
+  });
+
+  final int cardId;
+  final DateTime due;
+  final int scheduledDays;
+  final int elapsedDays;
+  final int reps;
+  final int lapses;
+  final DateTime lastReview;
+
+  @override
+  String toString() => 'SchedulerEntryEntity('
+      'cardId: $cardId, '
+      'due: $due, '
+      'scheduledDays: $scheduledDays, '
+      'elapsedDays: $elapsedDays, '
+      'reps: $reps, '
+      'lapses: $lapses, '
+      'lastReview: $lastReview'
+      ')';
+}
