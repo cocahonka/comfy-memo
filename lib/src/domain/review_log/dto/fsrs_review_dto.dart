9973dfab1d59b298e7a0a0f2abf50347041ab4c6
cocahonka <cocahonka@gmail.com>
2024-11-21 14:53:46 +0300

    add FSRS review log DTO and repository interface

---
+++ lib/src/domain/review_log/dto/fsrs_review_dto.dart
@@ -0,0 +1,16 @@
+import 'package:comfy_memo/src/domain/algorithm/entity/learning_rating.dart';
+import 'package:comfy_memo/src/domain/algorithm/usecase/fsrs_algorithm.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+final class FsrsReviewLogDto {
+  const FsrsReviewLogDto({
+    required this.review,
+    required this.rating,
+    required this.state,
+  });
+
+  final DateTime review;
+  final LearningRating rating;
+  final LearningState state;
+}
---
+++ lib/src/domain/review_log/repository/repository.dart
@@ -0,0 +1,10 @@
+import 'package:comfy_memo/src/domain/review_log/dto/fsrs_review_dto.dart';
+import 'package:comfy_memo/src/domain/review_log/entity/fsrs_review_log_entity.dart';
+
+abstract interface class IReviewLogRepository {
+  Future<void> logFsrs(int cardId, FsrsReviewLogDto params);
+
+  Future<List<FsrsReviewLogEntity>> fetchFsrs(int cardId);
+
+  Future<void> delete(int cardId);
+}
