de2ef0b21f1d2cea2ac4240c6309bc4d9be3a564
cocahonka <cocahonka@gmail.com>
2024-11-29 00:16:05 +0300

    design main list screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
+import 'package:comfy_memo/src/common/constants.dart';
+import 'package:comfy_memo/src/presentation/main_list/main_list_screen.dart';
+import 'package:comfy_memo/src/presentation/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+@immutable
+final class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    const themeMode = ThemeMode.system;
+    final textTheme = Theme.of(context).textTheme;
+    final theme = MaterialTheme(textTheme);
+
+    return MaterialApp(
+      themeMode: themeMode,
+      theme: theme.light(),
+      darkTheme: theme.dark(),
+      debugShowCheckedModeBanner: false,
+      title: Constants.appName,
+      home: const MainListScreen(),
     );
   }
 }
