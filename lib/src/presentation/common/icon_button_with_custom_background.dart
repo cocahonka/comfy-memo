6ffc43203cb51e8953db0b968991996eb63d3a07
cocahonka <cocahonka@gmail.com>
2024-11-30 16:49:18 +0300

    Add `IconButtonWithCustomBackground` widget

---
+++ lib/src/presentation/common/icon_button_with_custom_background.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+@immutable
+base class IconButtonWithCustomBackground extends StatelessWidget {
+  const IconButtonWithCustomBackground({
+    required IconButton iconButton,
+    required this.backgroundColor,
+    super.key,
+  }) : _iconButton = iconButton;
+
+  final IconButton _iconButton;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(
+          width: 40,
+          height: 40,
+          child: Ink(
+            decoration: ShapeDecoration(
+              shape: const CircleBorder(),
+              color: backgroundColor,
+            ),
+          ),
+        ),
+        _iconButton,
+      ],
+    );
+  }
+}
